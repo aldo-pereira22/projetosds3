3118e7def2e69a66625dcc96ca558b8687ca5082
Aldo Pereira <aldopereira.gyn@gmail.com>
2021-05-07 18:44:13 -0300

    Projeto Criado

---
+++ backend/src/test/java/com/dev22/dsvendas/DsvendasApplicationTests.java
@@ -0,0 +1,13 @@
+package com.dev22.dsvendas;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DsvendasApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
