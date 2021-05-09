035dadd3b239da601185f280b3d6a0c0eebc74bf
Aldo Pereira <aldopereira.gyn@gmail.com>
2021-05-09 10:56:20 -0300

    Criação dos componentes

---
+++ frontend/src/components/Footer/index.tsx
@@ -0,0 +1,18 @@
+import ImgDsDark from 'assets/img/ds-dark.svg'
+
+const  Footer = () => {
+    return (
+    <div>        
+        <footer className="footer mt-auto py-3 bg-dark">
+            <div className="container">
+                <p className="text-light">App desenvolvido por <a href="https://github.com/aldo-pereira22" target="_blank" rel="noreferrer">Aldo Pereira</a></p>
+                <p className="text-light"><small><strong>Semana Spring React</strong><br/>
+                Evento promovido pela escola DevSuperior: <a href="https://instagram.com/devsuperior.ig" target="_blank" rel="noreferrer">@devsuperior.ig</a></small></p>
+            </div>
+        </footer>
+    </div>
+    );
+  }
+  
+  export default Footer;
+  
